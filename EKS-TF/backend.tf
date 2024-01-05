b8e04c6efe86aa7e82aae4be815acf1b2cc44175
Maharshi Bhatnagar <119435144+Maharshibhatnagar@users.noreply.github.com>
2024-01-05 16:49:05 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "mario-is-not-a-trash-game" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
